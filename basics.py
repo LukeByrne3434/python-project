940556b17b8e50d83e9d72f4b1a082692b74b7a8
Luke Byrne <lukebyrne3434@gmail.com>
2021-02-09 10:54:18 +0000

    basics

---
+++ basics.py
@@ -0,0 +1,13 @@
+#Luke Byrne
+#Python basics
+#2021-02-07
+
+print("Hello, World!")
+
+a = 1
+b= 1.0
+s= "Hello, world from a string!"
+t= '"hello", from a different string'
+print(a,b,s,t)
+
+print(s[15:20:2])
